d0b11131e1c42e71883eebff221897d523cb8ad2
Jen Nerby <nerby.jennifer@gmail.com>
2021-11-03 23:14:12 +0000

    Add function

--- calculator.py
+++ calculator.py
@@ -1,11 +1,26 @@
 """CLI application for a prefix-notation calculator."""
-
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-# get equation from user
-equation = input("Your equation: ")
 
-#Tokenize equation
-token = equation.split(" ")
 
+# Repeat forever
+while True:
+
+    # Get equation from user
+    equation = input("Your equation: ")
+
+    # Tokenize equation
+    token = equation.split(" ")
+
+    arith = token[0]
+    num1 = float(token[1])
+    num2 = float(token[2])
+
+    # Quit if first token is q
+    if token[0] == "q":
+        quit()
+    else:
+        #Add function
+        if arith == "+":
+            print(add(num1, num2))
